fb665c1ee92626c80f07655ccaa3325e0b7edd49
FrancescoMarelli <alu0101161730@ull.edu.es>
2025-08-17 19:34:01 +0200

    CRUD finished

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -22,7 +22,47 @@ const createUser = async (req, res) => {
     }
 };
 
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user' });
+  }
+}
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating user' });
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting user' });
+  }
+}
+
+
 module.exports = {
   getAllUsers,
-  createUser
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,8 @@ const usersController = require('../controllers/users.controller');
 // Definir rutas
 router.get('/', usersController.getAllUsers);
 router.post('/new', usersController.createUser);
-
+router.get('/:id', usersController.getUserById);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
